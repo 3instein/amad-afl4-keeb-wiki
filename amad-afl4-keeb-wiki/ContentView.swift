d90d4a1d33db61e537f3442507e0cbedb608fdb3
3instein <reynaldi_kindarto@yahoo.com>
2022-05-24 08:07:59 +0700

    Initial Commit

---
+++ amad-afl4-keeb-wiki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amad-afl4-keeb-wiki
+//
+//  Created by Reynaldi Kindarto on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ amad-afl4-keeb-wiki/amad_afl4_keeb_wikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  amad_afl4_keeb_wikiApp.swift
+//  amad-afl4-keeb-wiki
+//
+//  Created by Reynaldi Kindarto on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct amad_afl4_keeb_wikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
