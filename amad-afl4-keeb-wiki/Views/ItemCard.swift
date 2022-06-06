421b5f8569b291347360405966f43714f550dab5
3instein <reynaldi_kindarto@yahoo.com>
2022-06-06 21:17:39 +0700

    Favorite logo fix on item card

--- amad-afl4-keeb-wiki/Views/ItemCard.swift
+++ amad-afl4-keeb-wiki/Views/ItemCard.swift
@@ -8,26 +8,33 @@
 import SwiftUI
 
 struct ItemCard: View {
-    private let padd: CGFloat = 32
+    @EnvironmentObject var modelData: ModelData
     var keyboard: Keyboard
+    
+    var keyboardIndex: Int {
+        modelData.keyboards.firstIndex(where: { $0.id == keyboard.id })!
+    }
+    
+    private let pad: CGFloat = 32
     
     var body: some View {
         VStack(alignment: .center) {
-            VStack(alignment: .trailing) {
+            
+            HStack {
+                Text(keyboard.name)
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .textCase(.uppercase)
+                .padding(EdgeInsets(top: 18, leading: 0, bottom: 0, trailing: 0))
                 if keyboard.is_favorite {
                     Image(systemName: "star.fill")
                         .foregroundColor(.yellow)
+                        .padding(EdgeInsets(top: 18, leading: 0, bottom: 0, trailing: 0))
                 }
             }
-            
-            Text(keyboard.name)
-                .font(.title3)
-                .fontWeight(.medium)
-                .textCase(.uppercase)
-                .padding(EdgeInsets(top: 18, leading: 0, bottom: 0, trailing: 0))
             Divider()
                 .background(.black)
-                .padding(EdgeInsets(top: 0, leading: padd, bottom: 0, trailing: padd))
+                .padding(EdgeInsets(top: 0, leading: pad, bottom: 0, trailing: pad))
             Text(keyboard.designer)
             Image(keyboard.image)
                 .resizable()
@@ -37,12 +44,15 @@ struct ItemCard: View {
 }
 
 struct ItemCard_Previews: PreviewProvider {
+    static let modelData = ModelData()
     static var keyboards = ModelData().keyboards
     
     static var previews: some View {
         Group {
             ItemCard(keyboard: keyboards[0])
+                .environmentObject(modelData)
             ItemCard(keyboard: keyboards[1])
+                .environmentObject(modelData)
         }
     }
 }
